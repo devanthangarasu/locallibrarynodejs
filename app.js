8cb1074d337f88f0dc45b1b2b4bed5b7e65d8b59
karthiyayini <karthiyayini@cogzidel.com>
2018-02-28 15:06:27 +0530

    commited new changed login and edit

---
+++ app.js
@@ -0,0 +1,112 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy =require('passport-local').Strategy;
+var compression = require('compression')
+var helmet = require('helmet')
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var catalog = require('./routes/catalog');
+
+var app = express();
+var expressValidator = require('express-validator')
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://dbuser:dbpass@ds147118.mlab.com:47118/local_library';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value){
+    var namespace = param.split('.'),
+    root = namespace.shift(),
+    formParam = root;
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return  {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/catalog',catalog);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Set Port
+app.set('port', (process.env.PORT || 3001));
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
+});
+
+module.exports = app;
